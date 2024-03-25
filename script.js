7883f478fdb6f33af62e33374cecb2890a18cb2d
hawu <37008343+haruai@users.noreply.github.com>
2024-03-25 19:33:16 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,30 @@
-const wrapper = document.querySelector(".wrapper");
-const question = document.querySelector(".question");
-const gif = document.querySelector(".gif");
-const yesBtn = document.querySelector(".yes-btn");
-const noBtn = document.querySelector(".no-btn");
-
-yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Yay, see you next year!";
-  gif.src =
-    "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
+$("#messageState").on("change", (x) => {
+    $(".message").removeClass("openNor").removeClass("closeNor");
+    if ($("#messageState").is(":checked")) {
+        $(".message").removeClass("closed").removeClass("no-anim").addClass("openNor");
+        $(".heart").removeClass("closeHer").removeClass("openedHer").addClass("openHer");
+        $(".container").css("background-image", "url('bgtatin.png')"); // Change the path to your image
+        console.log("Abrindo");
+    } else {
+        $(".message").removeClass("no-anim").addClass("closeNor");
+        $(".heart").removeClass("openHer").removeClass("openedHer").addClass("closeHer");
+        $(".container").css("background-image", "none"); // Revert to no background image
+        console.log("fechando");
+    }
 });
 
-noBtn.addEventListener("mouseover", () => {
-  const noBtnRect = noBtn.getBoundingClientRect();
-  const maxX = window.innerWidth - noBtnRect.width;
-  const maxY = window.innerHeight - noBtnRect.height;
+$(".message").on('webkitAnimationEnd oanimationend msAnimationEnd animationend', function(e) {
+    console.log("Animation End");
+    if ($(".message").hasClass("closeNor"))
+        $(".message").addClass("closed");
+    $(".message").removeClass("openNor").removeClass("closeNor").addClass("no-anim");
+});
 
-  const randomX = Math.floor(Math.random() * maxX);
-  const randomY = Math.floor(Math.random() * maxY);
-
-  noBtn.style.left = randomX + "px";
-  noBtn.style.top = randomY + "px";
+$(".heart").on('webkitAnimationEnd oanimationend msAnimationEnd animationend', function(e) {
+    console.log("Animation End");
+    if (!$(".heart").hasClass("closeHer"))
+        $(".heart").addClass("openedHer").addClass("beating");
+    else
+        $(".heart").addClass("no-anim").removeClass("beating");
+    $(".heart").removeClass("openHer").removeClass("closeHer");
 });
